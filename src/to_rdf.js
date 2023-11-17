c51efa8c2753ae56efc07160e825ca2140c82254
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2023-11-17 13:42:13 +0000

    Remove fixUri function again / Adding a trailing slash to the id seems to fix the issue  / Bump to version 0.0.6

--- src/to_rdf.js
+++ src/to_rdf.js
@@ -21,22 +21,21 @@ async function documentLoader (url) {
   nodeDocumentLoader(url)
 }
 
-function fixUris (description, id) {
-  // a bug in jsonld prevents the usage of urns as base uris
-  // we need to traverse over all @id and replace all urn:/
-
-  // instead of traversing the whole object we convert it to a string and fix all urns there.
-  const str = JSON.stringify(description).replace(/uri:/g, `${id}/`)
-  return JSON.parse(str)
+function ensureTrailingSlash (str) {
+  if (str.endsWith('/')) {
+    return str
+  } else {
+    return str + '/'
+  }
 }
 
 async function toRDF (description) {
   const expanded = await jsonld.expand(description, {
     documentLoader,
-    base: description.id
+    base: ensureTrailingSlash(description.id)
   })
 
-  return jsonld.toRDF(fixUris(expanded, description.id), {
+  return jsonld.toRDF(expanded, {
     format: 'application/n-quads'
   })
 }
